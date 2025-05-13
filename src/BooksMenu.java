fe921e03d8a15c22e14db60318054d92aa6a5752
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-13 19:58:48 -0400

    Provide user menu

--- src/BooksMenu.java
+++ src/BooksMenu.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class BooksMenu {
 
     public static void main(String s[]) {
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
         Book[] books = new Book[10];
         int bkIdx = 0;
         while(true) {
-            System.out.println("Press 1 to view books, 2 to add books, 3 to compare prices of books, any other key to exit");
+            System.out.println(	"Press 1 to view books, 2 to add books, "+
+                    "3 to compare prices of books, any other key to exit");
             String userAction = scanner.nextLine();
             if (userAction.equals("1")) {
                 for(int i=0;i<books.length;i++) {
@@ -20,31 +21,33 @@ public class BooksMenu {
                     System.out.println("10 books added already. Cannot add any more books!");
                     continue;
                 }
+                System.out.println("Which constructor do you want to use? Press 1 for default,"+
+                        "any other key for overloaded constructor");
+                String constructor = scanner.nextLine();
                 System.out.println("Enter book title");
                 String tmpTitle = scanner.nextLine();
                 System.out.println("Enter book author");
                 String tmpAuthor = scanner.nextLine();
                 System.out.println("Enter book price");
                 float tmpPrice = Float.parseFloat(scanner.nextLine());
-                Book bkTmp = new Book();
-                bkTmp.setTitle(tmpTitle);
-                bkTmp.setAuthor(tmpAuthor);
-                bkTmp.setPrice(tmpPrice);
-                books[bkIdx++] = bkTmp;
-            }
-            else if (userAction.equals("3")) {
-                // Prompt the user for the indices of the books to compare
+                if (constructor.equals("1")) {
+                    Book bkTmp = new Book();
+                    bkTmp.setTitle(tmpTitle);
+                    bkTmp.setAuthor(tmpAuthor);
+                    bkTmp.setPrice(tmpPrice);
+                    books[bkIdx++] = bkTmp;
+                } else {
+                    books[bkIdx++] = new Book(tmpTitle, tmpAuthor, tmpPrice);
+                }
+            } else if (userAction.equals("3")) {
                 System.out.println("Enter index of first book to compare");
                 int book1Idx = Integer.parseInt(scanner.nextLine());
                 System.out.println("Enter index of second book to compare");
                 int book2Idx = Integer.parseInt(scanner.nextLine());
-                // Check if the selected indices contain valid Book objects
-                if (books[book1Idx] != null && books[book2Idx] != null) {
-                    // Compare the books and print the details of the more expensive book
-                    System.out.println("The details of expensive book is \n" +
-                            getExpensiveBook(books[book1Idx], books[book2Idx]));
+                if (books[book1Idx] != null && books[book2Idx] != null ) {
+                    System.out.println("The details of expensive book is \n"+getExpensiveBook(books[book1Idx],books[book2Idx]));
                 } else {
-                    System.out.println("One of the books is null"); // Error message if an index is empty
+                    System.out.println("One of the books is null");
                 }
             } else {
                 break;
