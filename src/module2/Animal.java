421478fb00fc57f164ba588b8541f3acd002dbe1
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-15 22:41:50 -0400

    Implementing Polymorphism

---
+++ src/module2/Animal.java
@@ -0,0 +1,43 @@
+package module2;
+
+public class Animal {
+    private String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public String sound() {
+        return null;
+    }
+    public String toString() {
+        return name.concat(" says ").concat(sound());
+    }
+
+}
+
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    public String sound() {
+        return "Woof";
+    }
+}
+
+class Cat extends Animal {
+    public Cat(String name){
+        super(name);
+    }
+    public String sound() {
+        return "Meow";
+    }
+}
+
+class Cow extends Animal {
+    public Cow(String name){
+        super(name);
+    }
+    public String sound() {
+        return "Moo";
+    }
+}
