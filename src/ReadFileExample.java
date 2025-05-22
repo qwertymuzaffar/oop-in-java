b5eb362d39c4f3833c3c8a43c943b24723219a4f
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-22 11:19:50 -0400

    Practice Exercise

--- src/ReadFileExample.java
+++ src/ReadFileExample.java
@@ -6,18 +6,31 @@ import java.util.Scanner;
 
 public class ReadFileExample {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the name of the file you want to read.");
+        while (true) {
 
-        Path filePath = Paths.get(scanner.nextLine());
+            System.out.println("Enter the name of the file you want to read (or type 'exit' to quit):");
+            String userInput = scanner.nextLine();
 
-        try {
-            String content = Files.readString(filePath);
+            if (userInput.equals("exit")) {
+                System.out.println("Exiting the program. Goodbye!");
+                break;
+            } else {
+                Path filePath = Paths.get(userInput);
 
-            System.out.println(content);
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
+                try {
+                    String content = Files.readString(filePath);
+
+                    System.out.println(content);
+                } catch (IOException e) {
+                    System.err.println("Error reading file: " + e.getMessage());
+                }
+            }
         }
+
+        scanner.close();
+
     }
 }
