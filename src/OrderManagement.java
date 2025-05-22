4091c2623aa50bb49cd901980bd663d0cd0bec29
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-21 21:13:34 -0400

    Managing customer orders in an e-commerce application

---
+++ src/OrderManagement.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class OrderManagement {
+    private HashMap<Integer, String> orders;
+
+    public OrderManagement() {
+        orders = new HashMap<>();
+    }
+
+    public void addOrder(int orderId, String customerName) {
+        orders.put(orderId, customerName);
+    }
+
+    public void displayOrders() {
+        System.out.println("Customer Orders:");
+        for (int orderId : orders.keySet()) {
+            System.out.println("Order ID: " + orderId + ", Customer Name: " + orders.get(orderId));
+        }
+    }
+
+    public static void main(String[] args) {
+        OrderManagement orderManagement = new OrderManagement();
+        orderManagement.addOrder(101, "Alice");
+        orderManagement.addOrder(102, "Bob");
+        orderManagement.displayOrders();
+    }
+}
