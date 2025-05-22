85cef380ecdc5d63b383952ec9fdc8829869e1f9
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-22 15:52:32 -0400

    rename/delete a directory or file

--- src/DirectoryExplorer.java
+++ src/DirectoryExplorer.java
@@ -4,9 +4,91 @@ import java.io.IOException;
 
 public class DirectoryExplorer {
 
+    // Method to handle file management operations
+    private static void fileManagement(File file) {
+        // Display options to the user
+        System.out.println("\nPress 1 to rename the file," +
+                "\nPress 2 to delete the file," +
+                "\nAny other key to exit");
+
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.nextLine();
+
+        // Handle user choice
+        if (userChoice.equals("1")) {
+            // Get new name for the file
+            System.out.println("Enter the new name for the file " + file.getName());
+            String newfileName = scanner.nextLine();
+            // Attempt to rename the file
+            boolean changed = file.renameTo(new File(file.getParent(), newfileName));
+            //Print message about success or failure of file renaming
+            if (changed) {
+                System.out.println("Filename successfully changed");
+            } else {
+                System.out.println("Filename couldn't be changed!");
+            }
+        } else if (userChoice.equals("2")) {
+            // Delete the file
+            boolean deleted = file.delete();
+            //Print message about success or failure of file deletion
+            if (deleted) {
+                System.out.println("Filename successfully deleted");
+            } else {
+                System.out.println("Filename couldn't be deleted!");
+            }
+        }
+    }
+
+    // Method to handle directory management operations (list, rename, or delete)
+    private static void directoryManagement(File dirObj) {
+        // Display options to the user
+        System.out.println("\nPress 1 to list the directory," +
+                "\nPress 2 to rename the directory," +
+                "\nPress 3 to delete the directory," +
+                "\nAny other key to exit");
+
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.nextLine();
+
+        // Handle user choice
+        if (userChoice.equals("1")) {
+            // List the contents of the directory
+            String[] fileNames = dirObj.list();
+            if (fileNames.length == 0) {
+                System.out.println("The directory is empty!");
+            } else {
+                for (int i = 0; i < fileNames.length; i++) {
+                    System.out.println(fileNames[i]);
+                }
+            }
+        } else if (userChoice.equals("2")) {
+            // Rename the directory
+            System.out.println("Enter the new name for the directory " + dirObj.getName());
+            String newDirName = scanner.nextLine();
+            // Attempt to rename the directory
+            boolean changed = dirObj.renameTo(new File(dirObj.getParent(), newDirName));
+            //Print message about success or failure of dir renaming
+            if (changed) {
+                System.out.println("Directory name successfully changed");
+            } else {
+                System.out.println("Directory name couldn't be changed!");
+            }
+        } else if (userChoice.equals("3")) {
+            // Delete the directory
+            boolean deleted = dirObj.delete();
+            //Print message about success or failure of dir deletion
+            if (deleted) {
+                System.out.println("Directory successfully deleted");
+            } else {
+                System.out.println("Directory couldn't be deleted! It might not be empty");
+            }
+        }
+    }
+
     public static void main(String s[]) {
         Scanner scanner = new Scanner(System.in);
 
+        // Loop indefinitely until the user chooses to exit
         while (true) {
             // Display the menu options to the user
             System.out.println("\nPress 1 for File Management," + "\nAny other key to exit");
@@ -30,12 +112,16 @@ public class DirectoryExplorer {
                     // Check if the file is a file or directory
                     if (file.isFile()) {
                         System.out.println(fileName + " is a file");
+                        // Call the fileManagement method for file operations
+                        fileManagement(file);
                     } else {
                         System.out.println(fileName + " is a directory");
+                        // Call the directoryManagement method for directory operations
+                        directoryManagement(file);
                     }
                 } else {
                     // If the file or directory does not exist, prompt the user to create it
-                    System.out.println(fileName + " is a not a valid file or directory");
+                    System.out.println(fileName + " is not a valid file or directory");
                     System.out.println("To create a file with given name press 1\n"
                             + "To create a directory with given name press 2\n"
                             + "To do nothing and continue, press any other key");
@@ -45,23 +131,17 @@ public class DirectoryExplorer {
 
                     // Create a file
                     if (createChoice.equals("1")) {
-
                         // Get the parent directory
                         String parentDirStr = file.getParent();
+                        File parentDir = new File(parentDirStr);
 
-                        try {
-                            File parentDir = new File(parentDirStr);
-
-                            // Create the parent directory if it does not exist
-                            if (!parentDir.exists()) {
-                                boolean created = parentDir.mkdirs();
-                                if (!created) {
-                                    System.out.println("The parent directory could not be created");
-                                    continue;
-                                }
+                        // Create the parent directory if it does not exist
+                        if (!parentDir.exists()) {
+                            boolean created = parentDir.mkdirs();
+                            if (!created) {
+                                System.out.println("The parent directory could not be created");
+                                continue;
                             }
-                        } catch (NullPointerException e) {
-                            System.out.println("The parent directory doesn't exist");
                         }
 
                         // Create the file
