eadfe80d350e4d83e987e4e3c371130d2ca59ab2
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-25 17:13:25 -0400

    Practice Exercise

--- src/date/ZonedDateTimePrinter.java
+++ src/date/ZonedDateTimePrinter.java
@@ -19,8 +19,10 @@ public class ZonedDateTimePrinter {
                 "m for Minute of hour (0-59)\n" +
                 "mm for Zero-padded minute of hour (00-59)\n" +
                 "s for Second of minute (0-59), ss for Zero-padded second of minute (00-59)\n" +
-                "z for Time zone (for example, PDT, EST), zzz: Time zone (for example, Pacific Daylight Time, Eastern Standard Time)" +
-                "Z for Time zone offset (for example, +0800, -0500)");
+                "K for Hour of day (0-11), KK: Zero-padded hour of day (00-11), k: Hour of day (1-24)\n" +
+                "z for Time zone (for example, PDT, EST), zzz: Time zone (for example, Pacific Daylight Time, Eastern Standard Time)\n" +
+                "Z for Time zone offset (for example, +0800, -0500)\n" +
+                "a for displaying AM/PM");
 
 
         String strDateTimeFormat = scanner.nextLine();
