8c6c61ae25960a9de2ca48a24cfcc0fb5b335449
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-27 22:09:04 -0400

    Using the LocalTime class

---
+++ src/date/cheatsheet/LocalTimeExample.java
@@ -0,0 +1,10 @@
+package date.cheatsheet;
+
+import java.time.LocalTime;
+
+public class LocalTimeExample {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current time: " + currentTime);
+    }
+}
