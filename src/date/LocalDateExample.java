1f16aa2a6bac739a1865866aaaac71d66e8d376b
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-27 22:07:03 -0400

    Using the LocalDate class

---
+++ src/date/LocalDateExample.java
@@ -0,0 +1,10 @@
+package date;
+
+import java.time.LocalDate;
+
+public class LocalDateExample {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today's date: " + today);
+    }
+}
