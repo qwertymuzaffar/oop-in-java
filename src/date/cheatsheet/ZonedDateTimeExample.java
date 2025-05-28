bf46f7c25b3abd1bb3b7b0fa2b32c6c40e916482
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-05-27 22:27:23 -0400

    Creating a ZoneDateTime

--- src/date/cheatsheet/ZonedDateTimeExample.java
+++ src/date/cheatsheet/ZonedDateTimeExample.java
@@ -1,10 +1,12 @@
 package date.cheatsheet;
 
 import java.time.ZonedDateTime;
+import java.time.ZoneId;
 
 public class ZonedDateTimeExample {
     public static void main(String[] args) {
-        ZonedDateTime zonedNow = ZonedDateTime.now();
-        System.out.println("Current date and time with zone: " + zonedNow);
+        // Getting the current date and time in New York
+        ZonedDateTime newYorkTime = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        System.out.println("Current Date and Time in New York: " + newYorkTime);
     }
 }
